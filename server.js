6fba62019b57eb6b06ee6a169a67889bccafad1f
michaelzhuang <m.zhuang@arc.unsw.edu.au>
2014-05-13 10:47:14 +1000

    gitignore working with 'git rm -r --cached .'

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ use nodemon / supervisor to monitor
 */
 var express = require('express');
     stylus = require('stylus');
+    mongoose = require('mongoose');
+
 var app = express();
 
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
@@ -24,6 +26,15 @@ app.use(stylus.middleware(
 ));
 app.use(express.static(__dirname + '/public'));
 //end setting
+
+//start mongodb setting
+mongoose.connect('mongodb://localhost/multivision');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error...'));
+db.once('open', function callback(){
+   console.log('multivision db opened');
+});
+//end monogo db
 
 app.get('/partials/:partialPath', function(req, res){
     res.render('partials/' + req.params.partialPath);
